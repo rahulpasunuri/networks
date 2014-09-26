adaf14027f1a4f283c398073d5e729b755df5ceb
manishvr111 <manish.vr111@gmail.com>
2014-09-26 17:30:54 -0400

    updating bencode.cpp and adding RoadMap file

--- lab2/bencode.cpp
+++ lab2/bencode.cpp
@@ -9,200 +9,155 @@
 using namespace std;
 #define MAX_MATCHES 100 //The maximum number of matches allowed in a single string
 
-void match(regex_t *pexp, char *sz) 
-{
-	regmatch_t matches[MAX_MATCHES]; //A list of the matches in the string (a list of 1)
-	char token; int i=0; int data;
-	//regexec() returns 0 on match, otherwise REG_NOMATCH
-	while(regexec(pexp, sz, MAX_MATCHES, matches, 0) == 0)
-	{
-        int endIndex=matches[0].rm_eo;
-        //printf("\"%s\" matches characters %d - %d\n", sz, matches[0].rm_so, matches[0].rm_eo);								
-		if(sz[endIndex-1] == ':')
-		{
-            endIndex--;	    
-			char* integer = new char[endIndex];
-            for(int i=0;i<endIndex;i++)
-            {
-                integer[i]=sz[i];
-            }		
-            integer[endIndex]='\0';
-            int lengthOfString=atoi(integer);
-            printf("Integer is: %d\n",lengthOfString);
-            
-            char *s=new char[lengthOfString+1];
-            for(int j=0;j<lengthOfString;j++)
-            {
-                s[j]=sz[j+endIndex+1];
-            }
-            s[lengthOfString]='\0';
-            printf("String is: %s\n",s);
-            sz+=lengthOfString;
-            sz+=(endIndex); 
-    		sz++;
-		}
-        else
-        {
-            //cout<<"End Index is "<<endIndex<<endl;
-			char* var = new char[endIndex];
-            for(int i=0;i<endIndex;i++)
-            {
-                var[i]=sz[i];
-            }
-            var[endIndex]='\0';
-            cout<<"Var is: "<<var<<"\n";
-            sz+=(endIndex);
-        } 
+int pieceLength=0; int sm=0; char * buffer; bool isString=false;
 
-	} 	
-}
-bool isString=false;
+
 char* nextToken(regex_t *pexp, char* &sz, int *size) 
 {
-    isString=false;
+        isString=false;
 	regmatch_t matches[MAX_MATCHES]; //A list of the matches in the string (a list of 1)
-	char token; int i=0; int data;
+	 
 	//regexec() returns 0 on match, otherwise REG_NOMATCH
 	if(regexec(pexp, sz, MAX_MATCHES, matches, 0) == 0)
 	{
-        int endIndex=matches[0].rm_eo;
-        //printf("\"%s\" matches characters %d - %d\n", sz, matches[0].rm_so, matches[0].rm_eo);								
+        	int endIndex=matches[0].rm_eo;
+        								
 		if(sz[endIndex-1] == ':')
 		{
 		    isString=true;
-            endIndex--;	    
-			char* integer = new char[endIndex];
-            for(int i=0;i<endIndex;i++)
-            {
-                integer[i]=sz[i];
-            }		
-            integer[endIndex]='\0';
-            int lengthOfString=atoi(integer);
-                 *size = lengthOfString;
-            //printf("Integer is: %d\n",lengthOfString);
+            	    endIndex--;	    
+		    char* integer = new char[endIndex];
+            	    for(int i=0;i<endIndex;i++)
+            	    {
+                    	integer[i]=sz[i];
+                    }		
+                    integer[endIndex]='\0';
+                    int lengthOfString=atoi(integer);
+                    *size = lengthOfString;
 
-            char *s=new char[lengthOfString+1];
-            for(int j=0;j<lengthOfString;j++)
-            {
-                s[j]=sz[j+endIndex+1];
-            }
-            s[lengthOfString]='\0';
-            //printf("String is: %s\n",s);
-            sz+=lengthOfString;
-            sz+=(endIndex); 
-    		sz++;                        
-            return s;            
-		}
-        else
-        {          
-            //cout<<"End Index is "<<endIndex<<endl;
-			char* var = new char[endIndex];
-            for(int i=0;i<endIndex;i++)
-            {
-                var[i]=sz[i];
-            }
-            var[endIndex]='\0';
-            //cout<<"Var is: "<<var<<"\n";
-            sz+=(endIndex);
+                    char *s=new char[lengthOfString+1];
+                    for(int j=0;j<lengthOfString;j++)
+                    {
+                       s[j]=sz[j+endIndex+1];
+                    }
+                    s[lengthOfString]='\0';
             
-            return var;            
-        }        
+                    sz+=lengthOfString;
+                    sz+=(endIndex); 
+    		    sz++;                        
+                    return s;            
+		}
+        	else
+        	{          
+            
+	    		char* var = new char[endIndex];
+            		for(int i=0;i<endIndex;i++)
+            		{
+                		var[i]=sz[i];
+            		}
+            		var[endIndex]='\0';
+               		sz+=(endIndex);
+            
+            		return var;            
+        	}        
 	} 	
-    return NULL;
+        return NULL;
 }
 
+void token(char * text,regex_t *exp)
+{
+		      
+	if(strcmp(text,string("i").c_str())==0)
+	{   
+		cout<<"\n"<<atoi(nextToken(exp, buffer,&sm))<<"\t"<<"\n";
+				        
+		if(strcmp(nextToken(exp, buffer,&sm),string("e").c_str())!=0)
+		{
+			cout<<"parsing error\n";                          
+		}
+				          
+	}
+	else if(isString==true) 
+	{ 
+		cout<<"\t"<<"\n";
+		for(int i=0;i<sm;i++)
+		cout<<text[i];
+	}
 
+	else  if(!(strcmp(text,string("d").c_str())||strcmp(text,string("l").c_str())))
+	{
+				            
+		char* t =nextToken(exp, buffer, &sm);
+		while(strcmp(t,string("e").c_str())!=0)
+		{	
+			text = t; 
+			token(t,exp);
+		}
+		if(strcmp(text,string("d").c_str())==0)
+		{
+			char* tm =nextToken(exp, buffer, &sm);
+			while(strcmp(tm,string("e").c_str())!=0)
+			{
+				cout<<"\n";
+				for(int i=0;i<sm;i++)
+				cout<<tm[i];
+			}	
+
+		} 
+					   			    
+	}
+		   
+
+}						
+	
  
 int main(int argc, char* argv[]) 
-{            
-		int sm=0;  //    VARIABLE TO STORE THE SIZE VALUES OF LENGTH OF STRINGS...  
-		if(argc<2)
-		{
-		    cout<<"Invalid no. of arguments\n";
-		    exit(0);    
-		}     
-		
+{    
+    
+    regex_t exp; //Our compiled expression	    
+   
+    if(argc<2)
+    {
+        cout<<"Invalid no. of arguments\n";
+        exit(0);    
+    }     
 	    int rv;
-	    regex_t exp; //Our compiled expression	
+	    
 	    rv = regcomp(&exp, "([idel])|([0-9]+):|(-?[0-9]+)", REG_EXTENDED);
 	    if (rv != 0) 
 	    {
 		    printf("regcomp failed with %d\n", rv);
-			exit(0);
 	    }
 
-        fstream fp(argv[1],ios::in|ios::ate|ios::binary);
-        fp.seekg(0,ios::end);
-        int size = fp.tellg(); 
-                
-       	fp.seekg(0,ios::beg);
-        char * buffer = new char[size+1];
-        fp.read(buffer,size);
-        buffer[size] = '\0';
+       	    fstream fp(argv[1],ios::in|ios::ate|ios::binary);
+            fp.seekg(0,ios::end);
+            int size = fp.tellg();                 
+            fp.seekg(0,ios::beg);
+            buffer = new char[size+1];
+            fp.read(buffer,size);
+            buffer[size] = '\0';
          
 	    
 	    cout<<"\n\n\n..PRINTING OUT THE KEY VALUE PAIRS..\n\n";    
         while(true)
         {    
-             string P = "";
-             char *text=nextToken(&exp,buffer, &sm);
-            
+            string P = "";
+            char *text=nextToken(&exp,buffer, &sm);
             if(text==NULL)
             {
-                break;
+  	           break;
             }
             else 
-            {   
-                  if(!(strcmp(text,string("d").c_str())))
-	              {                            
-                       cout<<nextToken(&exp, buffer, &sm)<<"\t";
-                       char* t =nextToken(&exp, buffer,&sm);
-                       if(!strcmp(t,string("i").c_str()))
-                       {
-                            int s = atoi(nextToken(&exp, buffer, &sm));                            
-                            cout<<"\t"<<s<<endl;                            
-                       }
-                       else
-                       {
-                            cout<<t<<"\n";                   
-                       }
-                           
-                  }
-                  else if(!(strcmp(text,string("info").c_str())))
-                            
-                        cout<<text<<"\n";
-                    
-                  else  if(!(strcmp(text,string("name").c_str())))
-                  {
-                        cout<<text<<"\t\t";
-                        cout<<nextToken(&exp, buffer, &sm)<<"\n";
-                        cout<<nextToken(&exp, buffer, &sm)<<"\t";
-                        char* t =nextToken(&exp, buffer, &sm);
-                        if(!strcmp(t,string("i").c_str()))
-                        {
-                            int s = atoi(nextToken(&exp, buffer, &sm));
-                            
-                            cout<<s<<endl;                            
-                        }
-                            
-                  }      
-                  else  if(!(strcmp(text,string("pieces").c_str())))
-                  {
-                        cout<<text<<"\n";
-                        char * p =nextToken(&exp, buffer, &sm);
-                         // FOR LOOP TO PRINT OUT THE HASH..         
-                        for(int i=0;i<sm;i++)
-                            cout<<p[i];                                                                                            
-                  }
-                  else
-                        continue;
-             }                                                 
-        }                                                     
+            {
+  		   token(text,&exp);
+	    }   
+		              
+                             
+       }                                                     
       
-        //3. Free it
-	    regfree(&exp);
-	    
-	    return 0;
+       //3. Free it
+       regfree(&exp);
+       return 0;
 }
 
 
