4c1e73e14fdb1de2dea7962dd80bd404c568bf43
Rahul Pasunuri <rahul.pasunuri@gmail.com>
2014-10-22 02:34:15 -0400

    adding the length of the packet

--- lab3/wiretap.cpp
+++ lab3/wiretap.cpp
@@ -1,16 +1,82 @@
 #include<iostream>
 #include <pcap.h> //header file required for pcap...
-#include<string.h>
-#include<stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+//#include <netinet/ip.h> /* superset of previous *
 using namespace std;
 
+#include<string.h>
+#include<stdlib.h>
 
-/*
-void applyFilter()
-{
 
-}
-*/
+
+//define structures....
+/* Ethernet addresses are 6 bytes */
+#define ETHER_ADDR_LEN	6
+
+	/* Ethernet header */
+	struct sniff_ethernet {
+		u_char ether_dhost[ETHER_ADDR_LEN]; /* Destination host address */
+		u_char ether_shost[ETHER_ADDR_LEN]; /* Source host address */
+		u_short ether_type; /* IP? ARP? RARP? etc */
+	};
+
+	/* IP header */
+	struct sniff_ip {
+		u_char ip_vhl;		/* version << 4 | header length >> 2 */
+		u_char ip_tos;		/* type of service */
+		u_short ip_len;		/* total length */
+		u_short ip_id;		/* identification */
+		u_short ip_off;		/* fragment offset field */
+	#define IP_RF 0x8000		/* reserved fragment flag */
+	#define IP_DF 0x4000		/* dont fragment flag */
+	#define IP_MF 0x2000		/* more fragments flag */
+	#define IP_OFFMASK 0x1fff	/* mask for fragmenting bits */
+		u_char ip_ttl;		/* time to live */
+		u_char ip_p;		/* protocol */
+		u_short ip_sum;		/* checksum */
+		struct in_addr ip_src; /* source and dest address */
+		struct in_addr ip_dst;
+	};
+	#define IP_HL(ip)		(((ip)->ip_vhl) & 0x0f)
+	#define IP_V(ip)		(((ip)->ip_vhl) >> 4)
+
+	/* TCP header */
+	typedef u_int tcp_seq;
+
+	struct sniff_tcp {
+		u_short th_sport;	/* source port */
+		u_short th_dport;	/* destination port */
+		tcp_seq th_seq;		/* sequence number */
+		tcp_seq th_ack;		/* acknowledgement number */
+		u_char th_offx2;	/* data offset, rsvd */
+	#define TH_OFF(th)	(((th)->th_offx2 & 0xf0) >> 4)
+		u_char th_flags;
+	#define TH_FIN 0x01
+	#define TH_SYN 0x02
+	#define TH_RST 0x04
+	#define TH_PUSH 0x08
+	#define TH_ACK 0x10
+	#define TH_URG 0x20
+	#define TH_ECE 0x40
+	#define TH_CWR 0x80
+	#define TH_FLAGS (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+		u_short th_win;		/* window */
+		u_short th_sum;		/* checksum */
+		u_short th_urp;		/* urgent pointer */
+};
+
+//moooore structs
+/* ethernet headers are always exactly 14 bytes */
+#define SIZE_ETHERNET 14
+
+const struct sniff_ethernet *ethernet; /* The ethernet header */
+const struct sniff_ip *ip; /* The IP header */
+const struct sniff_tcp *tcp; /* The TCP header */
+u_char *payload; /* Packet payload */
+
+u_int size_ip;
+u_int size_tcp;
 
 void usage()
 {
@@ -44,10 +110,29 @@ char* parseArguments(int argc, char* argv[])
 
 int numPackets=0;
 
-void callback(u_char *, const struct pcap_pkthdr *, const u_char *)
+void callback(u_char *, const struct pcap_pkthdr *header, const u_char *packet) //the first argument is NULL in our case..
 {
-	cout<<"Callback called"<<endl;
+	cout<<"Length of the packet is "<<header->len<<endl;	
 	numPackets++;
+	
+	ethernet = (struct sniff_ethernet*)(packet);
+	ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
+	size_ip = IP_HL(ip)*4;
+	if (size_ip < 20) 
+	{
+		//printf("   * Invalid IP header length: %u bytes\n", size_ip);TODO
+		return;
+	}
+	tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
+	size_tcp = TH_OFF(tcp)*4;
+	if (size_tcp < 20) 
+	{
+		//printf("   * Invalid TCP header length: %u bytes\n", size_tcp); TODO
+		return;
+	}
+	payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);
+	
+	
 }
 
 
@@ -102,5 +187,8 @@ int main(int argc, char* argv[])
 		exit(1);
 	}
 	cout<<"Number of Packets processed is "<<numPackets<<endl;
+	
+	//close the handle
+	pcap_close(handle);
 	return 0;
 }
